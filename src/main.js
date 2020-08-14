2290d84fc7727a80968b104c97240153f7924e68
Mrorange <451607035@qq.com>
2020-08-14 21:33:08 +0800

    8.14

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入了element ui 组件库
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/Rank/index.vue
@@ -0,0 +1,333 @@
+<template>
+  <!-- 排行榜的大容器 -->
+  <div class="rankWrapper">
+    <!-- 左侧榜单的小容器 -->
+    <div class="rankList">
+      <!-- 云特色榜单 -->
+      <h2>云特色榜单</h2>
+      <!-- 榜单分类小容器 -->
+      <div class="ts">
+        <!-- 小容器里面的内容 -->
+        <div class="ts1">
+          <a href="javascript:;">
+            <img src="./imgs/1.png" alt />
+            <span class="s1">云音乐飙升榜</span>
+            <span class="s2">每天更新</span>
+          </a>
+        </div>
+      </div>
+      <div class="ts">
+        <!-- 榜单分类小容器 -->
+        <div class="ts1">
+          <a href="javascript:;">
+            <img src="./imgs/1.png" alt />
+            <span class="s1">云音乐飙升榜</span>
+            <span class="s2">每天更新</span>
+          </a>
+        </div>
+      </div>
+      <div class="ts">
+        <!-- 榜单分类小容器 -->
+        <div class="ts1">
+          <a href="javascript:;">
+            <img src="./imgs/1.png" alt />
+            <span class="s1">云音乐飙升榜</span>
+            <span class="s2">每天更新</span>
+          </a>
+        </div>
+      </div>
+      <div class="ts">
+        <!-- 榜单分类小容器 -->
+        <div class="ts1">
+          <a href="javascript:;">
+            <img src="./imgs/1.png" alt />
+            <span class="s1">云音乐飙升榜</span>
+            <span class="s2">每天更新</span>
+          </a>
+        </div>
+      </div>
+      <!-- 全球媒体榜单 -->
+      <h2>全球媒体榜</h2>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <!-- 小容器里面的内容 -->
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- 榜单分类小容器 -->
+      <div class="media">
+        <div class="ts">
+          <div class="ts1">
+            <a href="javascript:;">
+              <img src="./imgs/2.png" alt />
+              <span class="s1">云音乐说唱榜</span>
+              <span class="s2">刚刚更新</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 中间上面的容器 -->
+    <div class="rankTop">
+      <!-- 中上信息 -->
+      <div class="msg">
+        <img src="./imgs/3.png" alt="" />
+      </div>
+      <div class="msg2">
+        <h2>云音乐飙升榜</h2>
+        <div class="time">
+          <i class="el-icon-time"></i>
+          <span class="time2">最近更新: 08月16日 </span>
+          <span class="time3">(每天更新)</span>
+        </div>
+        <div class="msg3">
+          <a href="javascript;:" class="bf">
+            <img src="./imgs/button.png" alt="播放" />
+            播放</a
+          >
+          <a href="javascript;:" class="sc">
+            <!-- <img src="./imgs/button.png" alt="收藏"> --></a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Rank",
+};
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+// 排行榜的大容器样式
+.rankWrapper {
+  width: 980px;
+  height: 100%;
+  border: 1px solid #d3d3d3;
+  border-top: 1px solid #f1f0f0;
+  margin: 0 auto;
+  box-sizing: border-box;
+  position: relative;
+  // overflow: hidden;
+  // 左侧榜单的小容器样式
+  .rankList {
+    width: 240px;
+    // height: 100%;
+    border-right: 1px solid #d3d3d3;
+    box-sizing: border-box;
+    background: rgb(249, 249, 249);
+    padding: 30px 20px;
+    // position: absolute;
+    // float: left;
+    h2 {
+      padding: 10px 10px 12px 0px;
+    }
+    // 榜单分类小容器样式
+    .ts {
+      width: 200px;
+      height: 62px;
+      padding: 10px 0 10px 10px;
+      .ts1 {
+        position: relative;
+        // 图片样式
+        .img {
+          height: 40px;
+          width: 40px;
+          display: block;
+          position: absolute;
+        }
+        // 图片旁边文字样式
+        .s1 {
+          display: block;
+          color: rgb(0, 0, 0);
+          font-size: 12px;
+          position: absolute;
+          // margin-left: 10px;
+          left: 50px;
+          top: 0px;
+        }
+        // 图片旁边文字样式
+        .s2 {
+          display: block;
+          position: absolute;
+          font-size: 12px;
+          width: 100px;
+          color: rgb(153, 153, 153);
+          left: 50px;
+          top: 25px;
+        }
+      }
+    }
+  }
+  // 中间上面的容器
+  .rankTop {
+    width: 740px;
+    height: 238px;
+    position: absolute;
+    float: right;
+    top: 0;
+    left: 240px;
+    padding: 40px;
+    .msg {
+      height: 158px;
+      width: 158px;
+      border: 1px solid #000;
+      padding: 3px;
+    }
+    .msg2 {
+      position: absolute;
+      top: 50px;
+      left: 220px;
+      h2 {
+        line-height: 24px;
+        font-size: 20px;
+        font-weight: normal;
+      }
+      .el-icon-time {
+        display: inline-block;
+        font-size: 13px;
+        margin: 4px;
+      }
+      .time2 {
+        font-size: 12px;
+        color: #333;
+      }
+      .time3 {
+        color: #999;
+        font-size: 12px;
+      }
+      .msg3 {
+        width: 100px;
+        height: 31px;
+      }
+    }
+  }
+}
+</style>
