8518aac4f958042f109e2570ba1c7f0a11109120
Mrorange <451607035@qq.com>
2020-08-15 17:00:15 +0800

    8.15

--- src/pages/Rank/index.vue
+++ src/pages/Rank/index.vue
@@ -198,26 +198,137 @@
     <div class="rankTop">
       <!-- 中上信息 -->
       <div class="msg">
-        <img src="./imgs/3.png" alt="" />
+        <img src="./imgs/3.png" alt />
       </div>
       <div class="msg2">
         <h2>云音乐飙升榜</h2>
         <div class="time">
           <i class="el-icon-time"></i>
-          <span class="time2">最近更新: 08月16日 </span>
+          <span class="time2">最近更新: 08月16日</span>
           <span class="time3">(每天更新)</span>
         </div>
+        <!-- 雪碧图的播放收藏 -->
         <div class="msg3">
           <a href="javascript;:" class="bf">
-            <img src="./imgs/button.png" alt="播放" />
-            播放</a
-          >
-          <a href="javascript;:" class="sc">
-            <!-- <img src="./imgs/button.png" alt="收藏"> --></a
-          >
+            <span>播放</span>
+          </a>
+          <a href="javascript;:" class="tj"></a>
         </div>
+        <!-- 后面几个图标 -->
+        <a href="javascript;:" class="sc">
+          <span>(999)</span>
+        </a>
+        <a href="javascript;:" class="zf">
+          <span>转发</span>
+        </a>
+        <a href="javascript;:" class="xz">
+          <span>下载</span>
+        </a>
+        <a href="javascript;:" class="pl">
+          <span>(9999)</span>
+        </a>
       </div>
     </div>
+    <!-- 歌曲列表小框 -->
+    <div class="song">
+      <span class="s3">歌曲列表</span>
+      <span class="s4">100首歌曲</span>
+      <div class="s5">
+        播放:
+        <span class="s6" style="color:#c20c0c;font-weight: bold;">1891338368</span> 次
+      </div>
+    </div>
+    <!-- 歌曲列表的大容器 -->
+    <div class="songList">
+      <div class="t1"></div>
+      <div class="t2">标题</div>
+      <div class="t3">时长</div>
+      <div class="t4">歌手</div>
+      <table>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>
+            <i class="el-icon-video-play"></i> 天外来物
+          </td>
+          <td>04.17</td>
+          <td>薛之谦</td>
+        </tr>
+      </table>
+    </div>
+    <!-- 评论区小框 -->
+    <div class="comment">
+      <span class="c1">评论</span>
+      <span class="c2">共142461条评论</span>
+    </div>
+    <!-- 客户评论框 -->
+    <div class="comment2">
+      <img src="./imgs/1.png" class="c3" alt="用户头像" />
+      <form action="save.php" method="post">
+        <textarea class="c4" cols="80" rows="5"></textarea>
+        <br />
+        <input class="c5" type="submit" value="评论" name="submit" />
+      </form>
+    </div>
+    <!-- 精彩评论 -->
+    <h3 class="c6">精彩评论</h3>
+    <!-- 精彩评论区 -->
+    <div class="comment3"></div>
   </div>
 </template>
 <script>
@@ -298,7 +409,7 @@ export default {
     .msg {
       height: 158px;
       width: 158px;
-      border: 1px solid #000;
+      border: 1px solid rgb(197, 191, 191);
       padding: 3px;
     }
     .msg2 {
@@ -309,6 +420,8 @@ export default {
         line-height: 24px;
         font-size: 20px;
         font-weight: normal;
+        display: block;
+        margin-bottom: 15px;
       }
       .el-icon-time {
         display: inline-block;
@@ -323,11 +436,299 @@ export default {
         color: #999;
         font-size: 12px;
       }
+      // 雪碧图的播放+收藏样式
       .msg3 {
         width: 100px;
         height: 31px;
+        margin-top: 20px;
+        border-radius: 5%;
+        position: relative;
+        overflow: hidden;
+        display: inline-block;
+        .bf {
+          width: 70px;
+          height: 31px;
+          display: inline-block;
+          background-image: url(./imgs/button.png);
+          background-position: 0 -255px;
+          background-size: 195px;
+          margin-top: -15px;
+          span {
+            display: block;
+            line-height: 28px;
+            margin-left: 38px;
+            font-size: 12px;
+            color: white;
+          }
+        }
+        .bf:hover {
+          background-position: -1846px -255px;
+        }
+        .bf:active {
+          background-position: 0 -255px;
+        }
+        .tj {
+          width: 33px;
+          height: 34px;
+          background-position: 0px -721px;
+          display: inline-block;
+          background-image: url(./imgs/button.png);
+          position: absolute;
+          top: -1px;
+        }
+        .tj:hover {
+          background-position: -80px -721px;
+        }
+        .tj:active {
+          background-position: 0px -721px;
+        }
+      }
+      .sc {
+        background-image: url(./imgs/button.png);
+        display: inline-block;
+        width: 100px;
+        height: 30px;
+        background-position: 0 -225px;
+        position: absolute;
+        top: 83px;
+        left: 105px;
+        span {
+          font-size: 12px;
+          color: rgb(190, 190, 190);
+          padding-right: 2px;
+          padding-left: 35px;
+        }
+      }
+      .zf {
+        background-image: url(./imgs/button.png);
+        display: inline-block;
+        width: 100px;
+        height: 30px;
+        background-position: 0 -225px;
+        position: absolute;
+        top: 83px;
+        left: 210px;
+        // border-right: 1px solid #0c0c0c;
+        span {
+          font-size: 12px;
+          color: rgb(190, 190, 190);
+          padding-right: 2px;
+          padding-left: 35px;
+        }
+      }
+      .xz {
+        background-image: url(./imgs/button.png);
+        display: inline-block;
+        width: 100px;
+        height: 30px;
+        background-position: 0 -225px;
+        position: absolute;
+        top: 83px;
+        left: 300px;
+        span {
+          font-size: 12px;
+          color: rgb(190, 190, 190);
+          padding-right: 2px;
+          padding-left: 35px;
+        }
+      }
+      .pl {
+        background-image: url(./imgs/button.png);
+        display: inline-block;
+        width: 100px;
+        height: 30px;
+        background-position: 0 -225px;
+        position: absolute;
+        top: 83px;
+        left: 380px;
+        span {
+          font-size: 12px;
+          color: rgb(190, 190, 190);
+          padding-right: 2px;
+          padding-left: 35px;
+        }
       }
     }
+  }
+  // 歌曲列表小框
+  .song {
+    width: 670px;
+    height: 35px;
+    position: absolute;
+    left: 280px;
+    top: 238px;
+    border-bottom: 2px solid #c20c0c;
+    .s3 {
+      font-size: 20px;
+      line-height: 28px;
+      font-weight: normal;
+      color: #333;
+    }
+    .s4 {
+      margin: 9px 0 0 20px;
+      //  float: left;
+      color: #666;
+      font-size: 12px;
+    }
+    .s5 {
+      float: right;
+      color: #666;
+      font-size: 12px;
+    }
+  }
+  // 歌曲列表的大容器样式
+  .songList {
+    width: 670px;
+    border: 1px solid rgb(197, 191, 191);
+    position: absolute;
+    left: 280px;
+    top: 273px;
+    font-size: 12px;
+    color: rgb(138, 134, 134);
+    // padding: 0;
+    .t1 {
+      width: 77.2px;
+      height: 38.4px;
+      line-height: 38.4px;
+      // display: inline-block;
+      float: left;
+      border: 1.5px solid rgb(218, 217, 217);
+      text-align: center;
+      vertical-align: middle;
+      background-color: #f2f2f2;
+      border-left: none;
+    }
+    .t2 {
+      width: 327px;
+      height: 38.4px;
+      line-height: 38.4px;
+      display: inline-block;
+      border: 1.5px solid rgb(218, 217, 217);
+      text-align: center;
+      vertical-align: middle;
+      background-color: #f2f2f2;
+    }
+    .t3 {
+      width: 90px;
+      height: 38.4px;
+      line-height: 38.4px;
+      display: inline-block;
+      border: 1.5px solid rgb(218, 217, 217);
+      text-align: center;
+      vertical-align: middle;
+      background-color: #f2f2f2;
+    }
+    .t4 {
+      width: 173.6px;
+      height: 38.4px;
+      line-height: 38.4px;
+      display: inline-block;
+      border: 1.5px solid rgb(218, 217, 217);
+      text-align: center;
+      vertical-align: middle;
+      background-color: #f2f2f2;
+      border-right: none;
+    }
+    table {
+      width: 667px;
+      // margin: 50px auto;
+      border-collapse: collapse;
+      color: rgb(138, 134, 134);
+      font-size: 12px;
+
+      td {
+        // border: 1px solid black;
+        text-align: center;
+        height: 50px;
+        vertical-align: middle;
+      }
+      tr:nth-child(odd) {
+        color: rgb(138, 134, 134);
+        background-color: #f7f7f7;
+
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      tr:nth-child(even) {
+        font-size: 12px;
+        color: rgb(138, 134, 134);
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      .el-icon-video-play {
+        font-size: 17px;
+        margin: 5px;
+      }
+    }
+  }
+  // 评论区小框样式
+  .comment {
+    width: 670px;
+    height: 35px;
+    position: absolute;
+    left: 280px;
+    top: 770px;
+    border-bottom: 2px solid #c20c0c;
+    .c1 {
+      font-size: 20px;
+      line-height: 28px;
+      display: inline-block;
+      margin-right: 15px;
+    }
+    .c2 {
+      color: #666;
+      font-size: 12px;
+    }
+  }
+  // 客户评论框
+  .comment2 {
+    width: 670px;
+    height: 200px;
+    position: absolute;
+    left: 280px;
+    top: 820px;
+    display: flex;
+    textarea {
+      resize: none;
+    }
+    .c3 {
+      width: 50px;
+      height: 50px;
+      margin-right: 15px;
+    }
+    .c4 {
+      outline: none;
+      border: none;
+      border: 1px solid rgb(202, 201, 201);
+    }
+    .c5 {
+      margin-left: 550px;
+      background-color: #4391da;
+      color: white;
+      border-radius: 5%;
+      font-size: 12px;
+      font-family: Arial, Helvetica, sans-serif;
+      outline: none;
+      border: none;
+    }
+  }
+  .c6 {
+    position: absolute;
+    left: 280px;
+    top: 950px;
+    display: block;
+    font-size: 1.17em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+  }
+  // 精彩评论区
+  .comment3 {
+    border-top: 1px solid #cfcfcf;
+    position: absolute;
+    left: 280px;
+    top: 1000px;
+    width: 670px;
   }
 }
 </style>
